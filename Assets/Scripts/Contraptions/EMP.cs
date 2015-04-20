f9615cd78791087466e1ae9333142a18b6beefc9
Aggrathon <antonbjo@gmail.com>
2015-04-20 13:20:09 +0300

    Bug fix a

--- Assets/Scripts/Contraptions/EMP.cs
+++ Assets/Scripts/Contraptions/EMP.cs
@@ -5,9 +5,15 @@ public class EMP : MonoBehaviour {
 
     public float shieldDamage = 100f;
     public bool dontDespawn = false;
+    public GameObject blastPrefab;
 
     public void Blast()
     {
+        if (blastPrefab != null)
+        {
+            SimplePool.Spawn(blastPrefab, transform.position, transform.rotation);
+            SimplePool.Spawn(blastPrefab, transform.position, Quaternion.LookRotation(-transform.forward, transform.up));
+        }
         foreach (Collider col in Physics.OverlapSphere(transform.position, 1f))
         {
             Forcefield ff = col.transform.GetComponent<Forcefield>();
